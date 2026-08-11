0c68221589025a090b1a403425ad9c61f1b350d8
RAGHAV-MITTAL-upes <raghav.38772@stu.upes.ac.in>
2026-08-11 18:03:14 +0530

    Fix output statements for arithmetic operations

--- day-1-ques-2.c
+++ day-1-ques-2.c
@@ -7,8 +7,8 @@ int main()
     printf("enter the two numbers : ");
     scanf("%f %f", &a, &b);
     printf("sum = %.2f\n", a + b);
-    printf("sum = %.2f\n", a - b);
-    printf("sum = %.2f\n", a * b);
+    printf("difference = %.2f\n", a - b);
+    printf("product = %.2f\n", a * b);
     if (b != 0)
     {
        printf("quotient = %.2f\n", a / b);
