099cabe6bedb56503cc6ab95ea6e9905c3a653cd
RAGHAV-MITTAL-upes <raghav.38772@stu.upes.ac.in>
2026-08-11 16:51:04 +0530

    Create day-1-ques-1.c

---
+++ day-1-ques-1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+  int main(){
+int a , b ;
+    printf("enter the two numbers: ");
+    scanf("%d %d", &a, &b);
+int c =a + b;
+printf(" sum of %d and %d = %d", a, b, c);
+  return = 0;
+}
